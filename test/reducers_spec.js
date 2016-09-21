0ecfd68a5c451fd19fac7177fe389fa0622df1c8
Moritz Krog <moritzkrog@gmail.com>
2016-09-21 22:06:05 +0200

    Moved mislabeled reducers into reducer.js file

--- test/reducers_spec.js
+++ test/reducers_spec.js
@@ -1,10 +1,179 @@
-import { coreReducer } from '../app/reducers';
+import {
+  setTeachers,
+  addTeacher,
+  setLessons,
+  addLesson,
+  addStudentToLesson,
+  setStudentsForLesson,
+  coreReducer
+} from '../app/reducers';
 import constants from '../app/constants';
 import { expect } from 'chai';
-import { fromJS } from 'immutable';
+import { fromJS, Map } from 'immutable';
 
 
 describe('Reducer', () => {
+  let state = null;
+
+  beforeEach(() => {
+    state = Map();
+  });
+
+  describe('for teachers', () => {
+    let teachers = null;
+
+    beforeEach(() => {
+      teachers = [{
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc1',
+        name: 'Bertha Smith'
+      }, {
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc2',
+        name: 'The Dude'
+      }];
+    });
+
+    describe('setTeachers', () => {
+      it('sets the teachers for the state', () => {
+        const nextState = setTeachers(state, teachers);
+
+        expect( nextState ).to.equal(Map({ teachers: fromJS(teachers) }));
+      });
+    });
+
+    describe('addTeacher', () => {
+      it('adds a teacher to the state', () => {
+        state = state.set('teachers', fromJS([ teachers[0] ]));
+        const nextState = addTeacher(state, teachers[1]);
+
+        expect( nextState ).to.equal(Map({ teachers: fromJS(teachers) }));
+      });
+    });
+  });
+
+  describe('for lessons', () => {
+    let lessons = null;
+
+    beforeEach(() => {
+      lessons = [{
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc3',
+        subject: 'English 101',
+        teacher: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc1',
+        students: [],
+        date_starting: null,
+        date_ending: null,
+        weekday: 'Monday'
+      }, {
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc4',
+        subject: 'English 101',
+        teacher: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc1',
+        students: [],
+        date_starting: null,
+        date_ending: null,
+        weekday: 'Tuesday'
+      }];
+    });
+
+    describe('setLessons', () => {
+      it('sets the lessons for the state', () => {
+        const nextState = setLessons(state, lessons);
+
+        expect( nextState ).to.equal(Map({ lessons: fromJS(lessons) }));
+      });
+    });
+
+    describe('addLesson', () => {
+      it('adds the lesson to the state', () => {
+        state = state.set('lessons', fromJS([ lessons[0] ]));
+        const nextState = addLesson(state, lessons[1]);
+
+        expect( nextState ).to.equal(Map({ lessons: fromJS(lessons) }));
+      });
+    });
+  });
+
+  describe('for students', () => {
+    let students = null;
+    let lesson = null;
+
+    function getLesson(mixin = {}) {
+      return Object.assign({
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc3',
+        subject: 'English 101',
+        teacher: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc1',
+        students: [],
+        date_starting: null,
+        date_ending: null,
+        weekday: 'Monday'
+      }, mixin);
+    }
+
+    beforeEach(() => {
+      students = [{
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc5',
+        name: 'Robin'
+      }, {
+        id: 'ae535d61-26b0-4e26-8574-1d0e4e75bbc6',
+        name: 'Superboy'
+      }];
+    });
+
+    describe('setStudents', () => {
+      context('for an existing lesson', () => {
+        beforeEach(() => {
+          state = setLessons(state, [getLesson()]);
+        });
+
+        it('sets the students for the state', () => {
+          const nextState = setStudentsForLesson(state, getLesson(), students);
+          lesson = getLesson({ students });
+
+          expect( nextState ).to.equal(Map({ lessons: fromJS([lesson]) }));
+        });
+      });
+
+      context('for a non existing lesson', () => {
+        beforeEach(() => {
+          state = setLessons(state, []);
+        });
+
+        it('does not update the state', () => {
+          const nextState = setStudentsForLesson(state, getLesson(), students);
+
+          expect( nextState ).to.equal(Map({ lessons: fromJS([]) }));
+        });
+      });
+    });
+
+    describe('addStudent', () => {
+      context('for an existing lesson', () => {
+        beforeEach(() => {
+          lesson = getLesson({ students: [students[0]] });
+          state = setLessons(state, [lesson]);
+        });
+
+        it('adds the lesson to the state', () => {
+          const nextState = addStudentToLesson(state, lesson, students[1]);
+          lesson.students.push(students[1]);
+
+          expect( nextState ).to.equal(Map({ lessons: fromJS([lesson]) }));
+        });
+      });
+
+      context('for a non existing lesson', () => {
+        beforeEach(() => {
+          lesson = getLesson();
+          state = setLessons(state, []);
+        });
+
+        it('adds the lesson to the state', () => {
+          const nextState = addStudentToLesson(state, lesson, students[1]);
+
+          expect( nextState ).to.equal(Map({ lessons: fromJS([]) }));
+        });
+      });
+    });
+  });
+
   describe('coreReducer', () => {
     context('with an undefined state', () => {
       it('initialises the state', ()=> {
@@ -45,7 +214,7 @@ describe('Reducer', () => {
       let newState;
 
       beforeEach(() => {
-        const state = fromJS({
+        state = fromJS({
           teachers: [{ name: 'Bertha Smith', id: 'teacher-1' }]
         });
 
@@ -96,7 +265,7 @@ describe('Reducer', () => {
       let newState;
 
       beforeEach(() => {
-        const state = fromJS({
+        state = fromJS({
           lessons: [{ title: 'English 101', id: 'lesson-1' }]
         });
 
@@ -124,7 +293,7 @@ describe('Reducer', () => {
       context('for an existing lesson', () => {
         beforeEach(() => {
           const lesson = { title: 'English 101', students: [], id: 'lesson-1' };
-          const state = fromJS({ lessons: [ lesson ] });
+          state = fromJS({ lessons: [ lesson ] });
 
           newState = coreReducer(state, {
             type: constants.SET_STUDENTS_FOR_LESSON,
@@ -154,7 +323,7 @@ describe('Reducer', () => {
       context('for a non existing lesson', () => {
         beforeEach(() => {
           const lesson = { title: 'English 101', students: [], id: 'lesson-1' };
-          const state = fromJS({ lessons: [] });
+          state = fromJS({ lessons: [] });
 
           newState = coreReducer(state, {
             type: constants.SET_STUDENTS_FOR_LESSON,
@@ -183,7 +352,7 @@ describe('Reducer', () => {
             id: 'lesson-1',
             title: 'English 101',
             students: [{ name: 'The Dude', id: 'student-1' }] };
-          const state = fromJS({ lessons: [ lesson ] });
+          state = fromJS({ lessons: [ lesson ] });
 
           newState = coreReducer(state, {
             type: constants.ADD_STUDENT_TO_LESSON,
@@ -210,7 +379,7 @@ describe('Reducer', () => {
       context('for a non existing lesson', () => {
         beforeEach(() => {
           const lesson = { title: 'English 101', students: [], id: 'lesson-1' };
-          const state = fromJS({ lessons: [] });
+          state = fromJS({ lessons: [] });
 
           newState = coreReducer(state, {
             type: constants.ADD_STUDENT_TO_LESSON,
