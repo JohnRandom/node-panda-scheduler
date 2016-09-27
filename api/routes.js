4b7d1850a1518c5b9d5f6b25a2a9704877b5c4f9
Moritz Krog <moritzkrog@gmail.com>
2016-09-27 03:34:34 +0200

    Added routes for /lessons

--- api/routes.js
+++ api/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const teacherRoutes = require('./teachers');
+const lessonRoutes = require('./lessons');
 
 
 router.use('/teachers', teacherRoutes);
+router.use('/lessons', lessonRoutes);
 
 
 module.exports = router;
