80ace202f4c9bd4cb98927108bd079507ca1384c
Moritz Krog <moritzkrog@gmail.com>
2016-09-29 15:13:19 +0200

    Added catch-all for /api/ routes

--- api/routes.js
+++ api/routes.js
@@ -7,5 +7,15 @@ const lessonRoutes = require('./lessons');
 router.use('/teachers', teacherRoutes);
 router.use('/lessons', lessonRoutes);
 
+// Catch all, to avoid sending application page and irnform user with a
+// descriptive error message.
+router.all('*', (req, resp) => {
+  const url = req.originalUrl;
+
+  resp.status(404).json({
+    msg: 'Route "' + url + '" does not exist'
+  });
+});
+
 
 module.exports = router;
