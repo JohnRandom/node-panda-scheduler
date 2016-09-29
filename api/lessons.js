95930fafa8f6b396050f443118944eae3ef4da1e
Moritz Krog <moritzkrog@gmail.com>
2016-09-29 15:13:01 +0200

    Refactored /lessons and added specs

--- api/lessons.js
+++ api/lessons.js
@@ -1,93 +1,59 @@
-const knex = require('./db');
-const express = require('express');
-const router = express.Router();
+const objects = require('./objects');
+const router = require('express').Router();
 const codes = require('./codes');
-const uuid = require('node-uuid');
-
-
-const TIMESTAMP = /([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
 
 
 router.get('/', (req, res) => {
-  knex.select(
-    'lessons.id',
-    'lessons.name',
-    'lessons.description',
-    'lessons.weekday',
-    'lessons.time_starting',
-    'lessons.time_ending',
-    'lessons.teacher_id'
-  ).from('lessons')
-  .leftOuterJoin('lesson_attendees', 'lessons.id', 'lesson_id')
-  .leftOuterJoin('students', 'student_id', 'students.id')
-  .then((rows) => {
-    res.json(rows);
+  objects.lessons.all().then((data) => {
+    res.json(data);
   });
 });
 
 router.post('/', (req, res) => {
-  if (req.body.uuid) {
-    req.checkBody('id', 'Invalid UUID value for "id"').isUUID(4);
-  }
-  req.checkBody('name', 'Invalid text value for "name"').len(1, 255);
-  req.checkBody('weekday', 'Invalid int value for "weekday"', {
-    min: 0,
-    max: 6
-  });
-  req.checkBody('time_starting', 'Invalid text value for "time_starting"')
-    .matches(TIMESTAMP);
-  req.checkBody('time_ending', 'Invalid text value for "time_ending"')
-    .matches(TIMESTAMP);
-  req.checkBody('teacher_id', 'Invalid text value for "teacher_id"').isUUID(4);
-
+  const lesson = objects.lessons.validate(req);
   const errors = req.validationErrors();
+
   if (errors) {
     res.status(codes.UNPROCESSABLE).json(errors);
   } else {
-    knex.select('id').from('teachers').where({ id: req.body.teacher_id })
-      .then((rows) => {
-        if (rows.length > 0) {
-          const lesson = {
-            id: req.body.id || uuid.v4(),
-            name: req.body.name,
-            description: req.body.description,
-            weekday: req.body.weekday,
-            time_starting: req.body.time_starting,
-            time_ending: req.body.time_ending,
-            teacher_id: req.body.teacher_id
-          };
-          knex('lessons').insert(lesson).then(() => {
-            res.status(codes.CREATED).json(lesson);
-          });
-        } else {
-          res.status(codes.NOT_FOUND).json({
-            param: 'teacher_id',
-            msg: 'Teacher does not exist',
-            value: req.body.teacher_id
-          });
-        }
+    objects.teachers.get({ id: req.body.teacher_id }).then(() => {
+      objects.lessons.create(lesson).then(() => {
+        res.status(codes.CREATED).json(lesson);
       });
+    }).catch(() => {
+      res.status(codes.NOT_FOUND).json({
+        param: 'teacher_id',
+        msg: 'Teacher does not exist',
+        value: req.body.teacher_id
+      });
+    });
   }
 });
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
 
-  knex.first(
-    'id',
-    'name',
-    'description',
-    'weekday',
-    'time_starting',
-    'time_ending',
-    'teacher_id'
-  ).from('lessons').where({ id }).then((rows) => {
-    if (rows.length) {
-      const lesson = rows[0];
-      res.json(lesson);
-    } else {
-      res.status(codes.NOT_FOUND).json({});
-    }
+  objects.lessons.get({ id }).then((lesson) => {
+    res.json(lesson);
+  }).catch(() => {
+    res.status(codes.NOT_FOUND).json({
+      msg: 'The lesson with ID "' + req.params.id + '" doesn\'t exist'
+    });
+  });
+});
+
+router.post('/:id/attend', (req, res) => {
+  const id = req.params.id;
+  const student = objects.students.validate(req);
+
+  objects.lessons.get({ id }).then((row) => {
+    objects.lessons.attend(row, student).then(() => {
+      res.status(codes.CREATED).json(student);
+    });
+  }).catch(() => {
+    res.status(codes.NOT_FOUND).json({
+      msg: 'The lesson with ID "' + req.params.id + '" doesn\'t exist'
+    });
   });
 });
 
--- test/api/lessons.spec.js
+++ test/api/lessons.spec.js
@@ -53,4 +53,330 @@ describe('/lessons', () => {
       expect(response.body).to.deep.equal([lesson]);
     });
   });
+
+  describe('POST', () => {
+    let lesson;
+    let response;
+    let teacher;
+
+    context('with validate POST data', () => {
+      beforeEach((done) => {
+        resetDB().then(() => {
+          teacher = {
+            id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7',
+            name: 'The Dude'
+          };
+          lesson = {
+            id: '79689f8f-598e-45f3-b1e0-2396021d8a8a',
+            name: 'English 101',
+            weekday: 0,
+            description: 'Just another lesson.',
+            time_starting: '15:00:00',
+            time_ending: '16:30:00',
+            teacher_id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7'
+          };
+          knex('teachers').insert(teacher).then(() => {
+            $.post(host + '/api/lessons')
+              .set('Content-Type', 'application/json')
+              .send(JSON.stringify(lesson))
+              .end((err, resp) => {
+                response = resp;
+                done();
+              });
+          });
+        });
+      });
+
+      it('returns status code 201', () => {
+        expect(response.status).to.equal(201);
+      });
+
+      it('returns "accplication/json" as "Content-Type" header', () => {
+        const header = 'application/json; charset=utf-8';
+
+        expect(response.headers['content-type']).to.equal(header);
+      });
+
+      it('returns the lesson', () => {
+        expect(response.body).to.deep.equal(lesson);
+      });
+
+      it('creates a database entry for the lesson', (done) => {
+        knex('lessons').first('id').where({ id: lesson.id }).then((row) => {
+          expect(row).to.exist;
+          expect(row.id).to.equal(lesson.id);
+          done();
+        });
+      });
+    });
+
+    context('with invalidate POST data', () => {
+      beforeEach((done) => {
+        resetDB().then(() => {
+          teacher = {
+            id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7',
+            name: 'The Dude'
+          };
+          lesson = {
+            id: '79689f8f-598e-45f3-b1e0-2396021d8a8a',
+            weekday: 7,
+            teacher_id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7'
+          };
+          knex('teachers').insert(teacher).then(() => {
+            $.post(host + '/api/lessons')
+              .set('Content-Type', 'application/json')
+              .send(JSON.stringify(lesson))
+              .end((err, resp) => {
+                response = resp;
+                done();
+              });
+          });
+        });
+      });
+
+      it('returns status code 422', () => {
+        expect(response.status).to.equal(422);
+      });
+
+      it('returns "accplication/json" as "Content-Type" header', () => {
+        const header = 'application/json; charset=utf-8';
+
+        expect(response.headers['content-type']).to.equal(header);
+      });
+
+      it('returns the lesson', () => {
+        expect(response.body).to.deep.equal([
+          { param: 'name', msg: 'Invalid text value for "name"' },
+          { param: 'time_starting',
+            msg: 'Invalid text value for "time_starting"' },
+          { param: 'time_ending',
+          msg: 'Invalid text value for "time_ending"' }
+        ]);
+      });
+
+      it('does not create a database entry for the lesson', (done) => {
+        knex('lessons').select('id').where({ id: lesson.id }).then((rows) => {
+          expect(rows).to.be.empty;
+          done();
+        });
+      });
+    });
+
+    context('without an existing teacher', () => {
+      beforeEach((done) => {
+        resetDB().then(() => {
+          lesson = {
+            id: '79689f8f-598e-45f3-b1e0-2396021d8a8a',
+            name: 'English 101',
+            weekday: 0,
+            description: 'Just another lesson.',
+            time_starting: '15:00:00',
+            time_ending: '16:30:00',
+            teacher_id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7'
+          };
+          $.post(host + '/api/lessons')
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(lesson))
+            .end((err, resp) => {
+              response = resp;
+              done();
+            });
+        });
+      });
+
+      it('returns status code 201', () => {
+        expect(response.status).to.equal(404);
+      });
+
+      it('returns "accplication/json" as "Content-Type" header', () => {
+        const header = 'application/json; charset=utf-8';
+
+        expect(response.headers['content-type']).to.equal(header);
+      });
+
+      it('returns an error', () => {
+        expect(response.body).to.deep.equal({
+          'msg': 'Teacher does not exist',
+          'param': 'teacher_id',
+          'value': 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7'
+        });
+      });
+
+      it('does not create a database entry for the lesson', (done) => {
+        knex('lessons').select('id').where({ id: lesson.id }).then((rows) => {
+          expect(rows).to.be.empty;
+          done();
+        });
+      });
+    });
+  });
+});
+
+describe('/lessons/:id', () => {
+  describe('GET', () => {
+    let response;
+
+    context('with an exsting lesson', () => {
+      let lesson;
+      let teacher;
+
+      beforeEach((done) => {
+        resetDB().then(() => {
+          const url = '/api/lessons/79689f8f-598e-45f3-b1e0-2396021d8a8a';
+          teacher = {
+            id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7',
+            name: 'The Dude'
+          };
+          lesson = {
+            id: '79689f8f-598e-45f3-b1e0-2396021d8a8a',
+            name: 'English 101',
+            weekday: 0,
+            description: 'Just another lesson.',
+            time_starting: '15:00:00',
+            time_ending: '16:30:00',
+            teacher_id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7'
+          };
+          knex('teachers').insert(teacher).then(() => {
+            knex('lessons').insert(lesson).then(() => {
+              $.get(host + url).end((err, resp) => {
+                response = resp;
+                done();
+              });
+            });
+          });
+        });
+      });
+
+      it('returns status code 200', () => {
+        expect(response.status).to.equal(200);
+      });
+
+      it('returns "accplication/json" as "Content-Type" header', () => {
+        const header = 'application/json; charset=utf-8';
+
+        expect(response.headers['content-type']).to.equal(header);
+      });
+
+      it('returns the list of lessons', () => {
+        expect(response.body).to.deep.equal(lesson);
+      });
+    });
+  });
+});
+
+describe('/lessons/:id/attend', () => {
+  describe('GET', () => {
+    let response;
+
+    context('with an exsting lesson', () => {
+      let lesson;
+      let teacher;
+      let student;
+
+      beforeEach((done) => {
+        resetDB().then(() => {
+          teacher = {
+            id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7',
+            name: 'The Dude'
+          };
+          lesson = {
+            id: '79689f8f-598e-45f3-b1e0-2396021d8a8a',
+            name: 'English 101',
+            weekday: 0,
+            description: 'Just another lesson.',
+            time_starting: '15:00:00',
+            time_ending: '16:30:00',
+            teacher_id: 'aa3341d9-b8bd-454c-af89-7a79a0b4f9a7'
+          };
+          student = {
+            id: '65d8286c-66fb-40f0-90d4-b62720bd58b5',
+            name: 'Walter'
+          };
+          const url = '/api/lessons/' + lesson.id + '/attend';
+
+          knex('teachers').insert(teacher).then(() => {
+            knex('lessons').insert(lesson).then(() => {
+              $.post(host + url)
+                .set('Content-Type', 'application/json')
+                .send(JSON.stringify(student))
+                .end((err, resp) => {
+                  response = resp;
+                  done();
+                });
+            });
+          });
+        });
+      });
+
+      it('returns status code 201', () => {
+        expect(response.status).to.equal(201);
+      });
+
+      it('returns "accplication/json" as "Content-Type" header', () => {
+        const header = 'application/json; charset=utf-8';
+
+        expect(response.headers['content-type']).to.equal(header);
+      });
+
+      it('returns the student', () => {
+        expect(response.body).to.deep.equal(student);
+      });
+
+      it('registers the lesson attendance', (done) => {
+        knex('lesson_attendees').first('student_id', 'lesson_id')
+          .then((row) => {
+            expect(row).to.deep.equal({
+              student_id: '65d8286c-66fb-40f0-90d4-b62720bd58b5',
+              lesson_id: '79689f8f-598e-45f3-b1e0-2396021d8a8a'
+            });
+            done();
+          });
+      });
+    });
+
+    context('without an exsting lesson', () => {
+      let student;
+
+      beforeEach((done) => {
+        resetDB().then(() => {
+          student = {
+            id: '65d8286c-66fb-40f0-90d4-b62720bd58b5',
+            name: 'Walter'
+          };
+          const url = '/api/lessons/lesson_id/attend';
+
+          $.post(host + url)
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify(student))
+            .end((err, resp) => {
+              response = resp;
+              done();
+            });
+        });
+      });
+
+      it('returns status code 201', () => {
+        expect(response.status).to.equal(404);
+      });
+
+      it('returns "accplication/json" as "Content-Type" header', () => {
+        const header = 'application/json; charset=utf-8';
+
+        expect(response.headers['content-type']).to.equal(header);
+      });
+
+      it('returns an error', () => {
+        expect(response.body).to.deep.equal({
+          'msg': 'The lesson with ID \"lesson_id\" doesn\'t exist'
+        });
+      });
+
+      it('does not register the attendance', (done) => {
+        knex('lesson_attendees').select('student_id').then((rows) => {
+          expect(rows).to.be.empty;
+          done();
+        });
+      });
+    });
+  });
 });
